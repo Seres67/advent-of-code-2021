500e65ec1d86d9471831bc729fdf95e2e914767f
Laurent <laurent.haar@epitech.eu>
2021-12-07 03:28:56 +0100

    finished day2!

--- day2/main.cpp
+++ day2/main.cpp
@@ -5,8 +5,66 @@
 #include <iostream>
 #include "Utils.hpp"
 
+void forward(int *pos, int *depth, int aim, int i)
+{
+    *pos += i;
+    *depth += aim * i;
+}
+
+void down(int *depth, int i)
+{
+
+    *depth += i;
+}
+
+void up(int *depth, int i)
+{
+    *depth -= i;
+}
+
+int part1(const std::vector<std::string> &vals)
+{
+    int pos = 0;
+    int depth = 0;
+    int dummy = 0;
+
+    for (auto &i: vals)
+    {
+        int value = std::stoi(i.substr(i.find(' '), std::string::npos));
+
+        if (i.rfind("forward", 0) == 0)
+            forward(&pos, &dummy, 0, value);
+        else if (i.rfind("down", 0) == 0)
+            down(&depth, value);
+        else if (i.rfind("up") == 0)
+            up(&depth, value);
+    }
+    return depth * pos;
+}
+
+int part2(const std::vector<std::string> &vals)
+{
+    int pos = 0;
+    int depth = 0;
+    int aim = 0;
+
+    for (auto &i: vals)
+    {
+        int value = std::stoi(i.substr(i.find(' '), std::string::npos));
+
+        if (i.rfind("forward", 0) == 0)
+            forward(&pos, &depth, aim, value);
+        else if (i.rfind("down", 0) == 0)
+            down(&aim, value);
+        else if (i.rfind("up") == 0)
+            up(&aim, value);
+    }
+    return depth * pos;
+}
+
 int main()
 {
-    std::cout << "day2" << std::endl;
-    Utils::get_input("../input");
+    auto input = Utils::readFile("day2");
+    std::cout << "part1: " << part1(input) << std::endl;
+    std::cout << "part2: " << part2(input) << std::endl;
 }
